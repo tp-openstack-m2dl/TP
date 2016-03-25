3327fbc15ecb3c2e266e2bdf545a4f4fba7aee39
Serut <l.mieulet@gmail.com>
2016-03-25 17:21:18 +0100

    Clean UpdateRepartiteur.java

--- UpdateRepartiteur/src/main/java/UpdateRepartiteur.java
+++ UpdateRepartiteur/src/main/java/UpdateRepartiteur.java
@@ -12,36 +12,37 @@ import java.net.URL;
  * Created by Leo on 25/03/16.
  */
 public class UpdateRepartiteur {
-    public static class ClientRPC {
-        private XmlRpcClient client;
-        public ClientRPC(String ipRepartiteur, String portRepartiteur)  throws MalformedURLException, XmlRpcException {
-            // create configuration
-            XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
-            config.setServerURL(new URL("http://"+ipRepartiteur+":"+ portRepartiteur +"/xmlrpc"));
-            config.setEnabledForExtensions(true);
-            config.setConnectionTimeout(60 * 1000);
-            config.setReplyTimeout(60 * 1000);
+    private XmlRpcClient client;
+    public UpdateRepartiteur(String ipRepartiteur, String portRepartiteur)  throws MalformedURLException, XmlRpcException {
+        // create configuration
+        XmlRpcClientConfigImpl config = new XmlRpcClientConfigImpl();
+        config.setServerURL(new URL("http://"+ipRepartiteur+":"+ portRepartiteur +"/xmlrpc"));
+        config.setEnabledForExtensions(true);
+        config.setConnectionTimeout(60 * 1000);
+        config.setReplyTimeout(60 * 1000);
 
-            client = new XmlRpcClient();
+        client = new XmlRpcClient();
 
-            // use Commons HttpClient as transport
-            client.setTransportFactory(
-                    new XmlRpcCommonsTransportFactory(client));
-            // set configuration
-            client.setConfig(config);
-        }
-        public Object run(String command, Object[] params) throws MalformedURLException, XmlRpcException {
-            return client.execute(command, params);
-        }
+        // use Commons HttpClient as transport
+        client.setTransportFactory(
+                new XmlRpcCommonsTransportFactory(client));
+        // set configuration
+        client.setConfig(config);
     }
+    public Object run(String command, Object[] params) throws MalformedURLException, XmlRpcException {
+        return client.execute(command, params);
+    }
+
+
+
     public static void main(String[] args) throws MalformedURLException, XmlRpcException {
         if (args.length != 5) {
             throw new RuntimeException("update_repartiteur IPRepartiteur PortRepartiteur add|del Argument1 Argument2");
         }
         Object[] params = new Object[]
                 { args[3], args[4] };
-        ClientRPC clientRPC = new ClientRPC(args[0], args[1]);
-        clientRPC.run(args[2], params);
+        UpdateRepartiteur updateRepartiteur = new UpdateRepartiteur(args[0], args[1]);
+        updateRepartiteur.run(args[2], params);
 
     }
 }
