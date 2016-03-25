3ee96f9dc10397efb12358f387a84b2348045aa3
Julien Custoja <julien.custoja@gmail.com>
2016-03-25 16:26:23 +0100

    ELLIOT + JULIEN : on fait le repartiteur

---
+++ src/edu/m2dl/s10/arge/openstack/repartiteur/Repartiteur.java
@@ -0,0 +1,13 @@
+package edu.m2dl.s10.arge.openstack.repartiteur;
+
+/**
+ * Created by julien on 25/03/16.
+ */
+public class Repartiteur {
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("Il faut fournir le numéro du port");
+        }
+
+    }
+}
