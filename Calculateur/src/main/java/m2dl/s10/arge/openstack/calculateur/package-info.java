9b24a0918850e09b29a51cd6e3b760043691a806
Benoît Sauvère <benoit.sauvere@teevity.com>
2016-03-25 16:53:46 +0100

    Modification du la hiérarchie de package

--- Calculateur/src/main/java/m2dl/s10/arge/openstack/calculateur/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by julien on 25/03/16.
- */
-package java.m2dl.s10.arge.openstack.calculateur;
--- Repartiteur/src/main/java/m2dl/s10/arge/openstack/repartiteur/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Created by julien on 25/03/16.
- */
-package m2dl.s10.arge.openstack.repartiteur;
