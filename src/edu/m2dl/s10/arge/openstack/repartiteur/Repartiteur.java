f002c085df3ec75a17fa7cad071b10e2e0374189
Julien Custoja <julien.custoja@gmail.com>
2016-03-25 16:45:22 +0100

    En attente du calculateur

--- src/edu/m2dl/s10/arge/openstack/repartiteur/Repartiteur.java
+++ src/edu/m2dl/s10/arge/openstack/repartiteur/Repartiteur.java
@@ -1,9 +1,11 @@
 package edu.m2dl.s10.arge.openstack.repartiteur;
 
+import java.util.Map;
+
 /**
  * Created by julien on 25/03/16.
  */
-public class Repartiteur extends Runnable {
+public class Repartiteur implements Runnable {
     public static void main(String[] args) {
         if(args.length != 1) {
             System.out.println("Il faut fournir le numéro du port");
@@ -19,14 +21,34 @@ public class Repartiteur extends Runnable {
     }
 
     private String port;
-    List<Calculateur> lesCalculateurs;
+    Map<Calculateur> lesCalculateurs;
 
     @Override
     public void run() {
+
+        while(true) {
+
+            // Serveur : Récupérer les requetes des clients
+
+
+            // Serveur : Récupérer les requetes de l'updateRepartiteur
+
+
+            // Client : Contacter les Calculateur
+
+        }
 
     }
 
     public Repartiteur(String port) {
         this.port = port;
+    }
+
+    public void add(String ip, String port) {
+
+    }
+
+    public void del(String ip, String port) {
+
     }
 }
